ea1da57506a732fa68cbba107390d269a7318677
Amit Panwar <21213888+amitpanwarIndia@users.noreply.github.com>
2024-04-05 00:17:14 +0530

    version 2.0

--- finetuned_restnet.py
+++ finetuned_restnet.py
@@ -22,4 +22,31 @@ resnet18.fc = nn.Linear(num_ftrs, 10)  # 10 classes for FashionMNIST
 
 # Define loss function and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(resnet18.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.SGD(resnet18.parameters(), lr=0.001, momentum=0.9
+
+# Version 2.0: Fine-tune the model
+resnet18_ft = models.resnet18(pretrained=True)
+num_ftrs_ft = resnet18_ft.fc.in_features
+resnet18_ft.fc = nn.Linear(num_ftrs_ft, 10)  # Change output to 10 classes
+
+criterion_ft = nn.CrossEntropyLoss()
+optimizer_ft = optim.SGD(resnet18_ft.parameters(), lr=0.001, momentum=0.9)
+
+def fine_tune_model(model, criterion, optimizer, dataloader, num_epochs=5):
+    model.train()
+        for epoch in range(num_epochs):
+                running_loss = 0.0
+                        for i, data in enumerate(dataloader, 0):
+                                    inputs, labels = data
+                                                optimizer.zero_grad()
+                                                            outputs = model(inputs)
+                                                                        loss = criterion(outputs, labels)
+                                                                                    loss.backward()
+                                                                                                optimizer.step()
+                                                                                                            running_loss += loss.item()
+                                                                                                                    print(f"Epoch {epoch+1}, Loss: {running_loss/len(trainloader)}")
+
+                                                                                                                    fine_tune_model(resnet18_ft, criterion_ft, optimizer_ft, trainloader)
+
+                                                                                                                    # Save the fine-tuned model
+                                                                                                                    torch.save(resnet18_ft.state_dict(), 'resnet18_fashionmnist_v2.pth'))
